d69e14b286da4b8449e1a17eadcf95dddd5e3278
Bill Xu <billxbf@gmail.com>
2025-02-05 17:12:46 -0800

    1

--- README.md
+++ README.md
@@ -3,20 +3,20 @@ One step beyond Deepseek R1 on Mathematical Reasoning.
 
 
 ## Plan
-1. OTS eval of `qwen32-distill` q4 with vllm, with consensus.
+1. OTS eval of `qwen32b-distill` q4 with vllm, with consensus.
 2. Specialization: Collect hard math dataset and use R1 for rejection sampling + SFT
     - Collect QA data from huggingface, AoPS, etc.
     - Use R1 for data labeling ([distilabel](https://github.com/argilla-io/distilabel))
-    - Rejection Sampling + SFT, producing `qwen32-distill-math`.
+    - Rejection Sampling + SFT, producing `qwen32b-distill-math`.
 3. Exploration (RL): GRPO ([Trainer](https://huggingface.co/docs/trl/main/en/grpo_trainer))
     - Custom reward function (accuracy + length penalty?)
-    - Expand `qwen32-distill-math` potentials in reasoning space and produce `qwen32-distill-math-rl`.
+    - Expand `qwen32b-distill-math` potentials in reasoning space and produce `qwen32b-distill-math-rl`.
 
 4. Exploitation: Further supervise on selected trajectories.
-    - Collect synthetic data from `qwen32-distill-math-rl`.
+    - Collect synthetic data from `qwen32b-distill-math-rl`.
     - SFT/DPO on filtered generations that's
         - accurate
         - succinct 
         - truthful 
 
-        and produce `qwen32-distill-math-rl-sft`
+        and produce `qwen32b-distill-math-rl-sft`
---
+++ utils.py
@@ -0,0 +1,40 @@
+import re
+from trl import DataCollatorForCompletionOnlyLM
+
+R1PROMPT = """A conversation between User and Assistant. The user asks a question, and the Assistant solves it.
+The assistant first thinks about the reasoning process in the mind and then provides the user \
+with the answer. The reasoning process and answer are enclosed within <think> </think> and \
+<answer> </answer> tags, respectively, i.e., <think> reasoning process here </think> \
+<answer> answer here </answer>. User: Think carefully and solve the following math problem. Please reason step by step, and put your final answer within \\boxed{{}}. 
+{problem} 
+Assistant: <think>\n"""
+
+
+def format_prompt(example):
+    output_text = []
+    for i in range(len(example["problem"])):
+        text = R1PROMPT.format(problem=example["problem"][i])
+        output_text.append(text)
+    return output_text
+
+
+def get_trainer_collator(tokenizer):
+    response_template = "Assistant: <think>"
+    return DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
+
+
+def extract_answer(answer):
+    boxed_pattern = r'\\boxed{(\d+)}'
+    boxed_match = re.search(boxed_pattern, answer)
+
+    if boxed_match:
+        return int(boxed_match.group(1))
+
+    # If no boxed number found, find the last number in the text
+    number_pattern = r'\d+'
+    numbers = re.findall(number_pattern, answer)
+
+    if numbers:
+        return int(numbers[-1])
+
+    return 0
